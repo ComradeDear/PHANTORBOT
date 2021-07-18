17fad6eb677aaca55b38e79c22fe372290adaf5d
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-18 16:49:04 +0530

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
 os.system("git clone https://github.com/ComradeDear/PHANTORBOT")
-os.chdir("phanterbot")
+os.chdir("phantorbot")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
